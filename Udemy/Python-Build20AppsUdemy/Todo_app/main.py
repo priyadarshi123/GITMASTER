253fe6e9cbf5e412992fad8c2183e952e73e58a6
Priyadarshi <priyadarshi.k09@gmail.com>
2026-02-11 12:42:08 +0800

    Updated

--- Udemy/Python-Build20AppsUdemy/Todo_app/main.py
+++ Udemy/Python-Build20AppsUdemy/Todo_app/main.py
@@ -3,14 +3,23 @@ user_prompt = "Enter a todo: "
 todos=[]
 
 while True:
-    user_action = input("Type add, show or exit:").strip().lower()
+    user_action = input("Type add, show, edit  or exit:").strip().lower()
     match user_action:
         case "add":
             todo = input(user_prompt)
             todos.append(todo.capitalize())
         case "show":
-            for todo in todos:
-                print(todo.title())
+            for index,item in enumerate(todos):
+                print(f"{index + 1}-{item}")
+        case "edit":
+            number = int(input("which element you want to edit:"))
+            number = number - 1
+            new_todo = input("Enter new Todo:")
+            todos[number] = new_todo
+        case "complete":
+            number = int(input("Enter element number to remove (completed item)"))
+            todos.pop(number-1)
+            print("Item removed")
         case "exit":
             break
         case whatever:
