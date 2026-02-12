6c2b5a3f3684c145a2019c14d09693e2107cb23e
Priyadarshi <priyadarshi.k09@gmail.com>
2026-02-12 11:57:19 +0800

    latest

--- Udemy/Python-Build20AppsUdemy/Todo_app/main.py
+++ Udemy/Python-Build20AppsUdemy/Todo_app/main.py
@@ -1,16 +1,21 @@
 user_prompt = "Enter a todo: "
 
-todos=[]
+
 
 while True:
     user_action = input("Type add, show, edit  or exit:").strip().lower()
     match user_action:
         case "add":
-            todo = input(user_prompt)
-            todos.append(todo.capitalize())
+            todo = input(user_prompt) + '\n'
+            file = open("todos.txt",'a')
+            file.writelines(todo)
+            file.close()
         case "show":
+            file = open("todos.txt",'r')
+            todos = file.readlines()
             for index,item in enumerate(todos):
-                print(f"{index + 1}-{item}")
+                print(f"{index + 1}-{item}".strip('\n'))
+            file.close()
         case "edit":
             number = int(input("which element you want to edit:"))
             number = number - 1
