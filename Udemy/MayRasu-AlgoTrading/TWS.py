908ad19aa8ce0fb69a4f3d1b35edb744c3250b98
Priyadarshi <priyadarshi.k09@gmail.com>
2026-02-25 23:53:03 +0800

    Latest

---
+++ Udemy/MayRasu-AlgoTrading/TWS.py
@@ -0,0 +1,42 @@
+from ibapi.client import EClient
+from ibapi.wrapper import EWrapper
+from ibapi.contract import Contract
+import threading
+import time
+
+class TradingApp(EWrapper, EClient):
+    def __init__(self):
+        EClient.__init__(self,self)
+
+    def error(self, reqId, errorCode, errorString):
+        print("Error occurred: {} -  {} - {}".format(reqId,errorCode,errorString))
+
+    def contractDetails(self, reqId, contractDetails):
+        print("reqID: {}, contract:{}".format(reqId, contractDetails))
+
+
+
+def websocket_con():
+        app.run()
+        #event.wait()
+        #if event.is_set():
+        #    app.disconnect()
+
+event = threading.Event()
+app = TradingApp()
+app.connect("127.0.0.1", 7497, clientId=0)
+
+con_thread = threading.Thread(target=websocket_con)
+con_thread.start()
+time.sleep(1)
+
+contract = Contract()
+contract.symbol = "AAPL"
+contract.secType = "STK"
+contract.currency = "USD"
+contract.exchange = "SMART"
+
+app.reqContractDetails(1, contract)
+time.sleep(5)
+#event.set()
+app.disconnect()
