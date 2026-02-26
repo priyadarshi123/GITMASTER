e5bba8fbf768e67604e43063239f916c08edbc6f
Priyadarshi <priyadarshi.k09@gmail.com>
2026-02-27 01:17:15 +0800

    Latest

--- Udemy/MayRasu-AlgoTrading/TWS.py
+++ Udemy/MayRasu-AlgoTrading/TWS.py
@@ -1,8 +1,14 @@
 from ibapi.client import EClient
+from ibapi.common import BarData
 from ibapi.wrapper import EWrapper
 from ibapi.contract import Contract
 import threading
 import time
+
+#Paper TWS	7497
+#Live TWS	7496
+#Paper Gateway	4002
+#Live Gateway	4001
 
 class TradingApp(EWrapper, EClient):
     def __init__(self):
@@ -14,29 +20,37 @@ class TradingApp(EWrapper, EClient):
     def contractDetails(self, reqId, contractDetails):
         print("reqID: {}, contract:{}".format(reqId, contractDetails))
 
-
+    def historicalData(self, reqId: int, bar: BarData):
+        print("reqID: {}, bar:{}".format(reqId, bar))
 
 def websocket_con():
         app.run()
+        #print(app.isConnected())
         #event.wait()
         #if event.is_set():
         #    app.disconnect()
 
-event = threading.Event()
+#event = threading.Event()
 app = TradingApp()
-app.connect("127.0.0.1", 7497, clientId=0)
+#app.connect("127.0.0.1", 7497, clientId=0)
+app.connect("127.0.0.1", 4002, clientId=0)
 
 con_thread = threading.Thread(target=websocket_con)
 con_thread.start()
-time.sleep(1)
+time.sleep(2)
 
 contract = Contract()
 contract.symbol = "AAPL"
 contract.secType = "STK"
 contract.currency = "USD"
-contract.exchange = "SMART"
+contract.exchange = "NASDAQ"
 
-app.reqContractDetails(1, contract)
-time.sleep(5)
+ContractDetails = app.reqContractDetails(1, contract)
+
+Historical_data = app.reqHistoricalData(12,contract=contract, durationStr="1 W", barSizeSetting="1 day", whatToShow="TRADES", useRTH=True, endDateTime='', formatDate=1 , keepUpToDate=True, chartOptions=[])
+#time.sleep(5)
+#print(Historical_data)
+#time.sleep(5)
 #event.set()
+time.sleep(30)
 app.disconnect()
